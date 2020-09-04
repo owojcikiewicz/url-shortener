731b25f5107388a317fe68a5b5f63eaa1db62df9
Oskar <saturdaysheroes.business@gmail.com>
2020-09-04 23:40:07 +0200

    added basic config

--- config/config.go
+++ config/config.go
@@ -2,4 +2,39 @@ package config
 
 import (
 	"github.com/joho/godotenv"
+	"log"
+	"os"
 )
+
+type Config struct {
+	Port string
+	Password string
+	DB *DBConfig
+}
+
+type DBConfig struct {
+	Username string
+	Password string
+	Host string
+	Port string
+	Name string
+}
+
+func LoadConfig() *Config {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Couldn't load .env file!")
+	}
+
+	return &Config{
+		Port: os.Getenv("APP_PORT"),
+		Password: os.Getenv("APP_PASSWORD"),
+		DB: &DBConfig{
+			Username: os.Getenv("DB_USERNAME"),
+			Password: os.Getenv("DB_PASSWORD"),
+			Host: os.Getenv("DB_HOST"),
+			Port: os.Getenv("DB_PORT"),
+			Name: os.Getenv("DB_NAME"),
+		},
+	}
+}
