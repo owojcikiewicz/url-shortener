84b72d1d83af3d285eb35fe14a8151d0683d23b2
Oskar <saturdaysheroes.business@gmail.com>
2020-09-05 01:18:54 +0200

    added wip router for adding links

--- app/routes.go
+++ app/routes.go
@@ -3,23 +3,33 @@ package app
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"log"
 )
 
 func (app *App) InitializeRoutes(port string) error {
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
 
-	router.GET("/u/:id", func(c *gin.Context) {
-		var link Link
-		id := c.Param("id")
-
-		err := app.DB.First(&link, id).Error
+	router.POST("/upload", func(c *gin.Context) {
+		data, err := c.GetRawData()
 		if err != nil {
-			log.Fatal(err)
 			return
 		}
-		fmt.Println(link)
+		fmt.Println(data)
+	})
+
+	router.GET("/:id", func(c *gin.Context) {
+		link := Link{}
+		id := c.Param("id")
+
+		err := app.DB.Where("id = ?", id).First(&link).Error
+		if err != nil {
+			c.String(404, "404 - Link Not Found")
+			return
+		}
+
+		c.Redirect(307, link.URL)
+		link.Views = link.Views + 1
+		app.DB.Save(&link)
 	})
 
 	router.Run(fmt.Sprintf(":%s", port))
