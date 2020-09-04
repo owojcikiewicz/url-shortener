0fc644f20cde2980409a206f3eee15e33caab92a
Oskar <saturdaysheroes.business@gmail.com>
2020-09-05 00:55:56 +0200

    added app.go

---
+++ app/app.go
@@ -0,0 +1,23 @@
+package app
+
+import (
+	"gorm.io/gorm"
+	"log"
+	"url-shortener/config"
+)
+
+type App struct {
+	DB *gorm.DB
+}
+
+func (app *App) Run(config *config.Config) {
+	err := app.InitializeDatabase(config)
+	if err != nil {
+		log.Fatal("A database error occurred: ", err)
+	}
+
+	err = app.InitializeRoutes(config.Port)
+	if err != nil {
+		log.Fatal("A route error occurred: ", err)
+	}
+}
